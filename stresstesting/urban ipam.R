4da8851dff70dea7b78ce7c3418a15f24d379178
Michael-Studivan <studivanms@gmail.com>
2023-09-20 22:57:15 -0400

    stats output code

--- stresstesting/urban ipam.R
+++ stresstesting/urban ipam.R
@@ -3,20 +3,26 @@
 library(ggplot2)
 library(ggpubr)
 library(dplyr)
+library(tidyr)
 library(rstatix)
 library(MASS)
 
 
 #### data import ####
 
-ipam <- read.csv(file="ipam.csv", head=T)
+ipam <- read.csv(file="urban ipam.csv", head=T)
 str(ipam)
 
+# changing genotype numbers to be distinct across sites/species
+genotypes <- read.csv(file="genotype lookup table.csv", head=T)
+genotypes
+
+ipam %>%
+  inner_join(genotypes, by = "sampleID") -> ipam
+
 # coding factors
-ipam$genotype <- as.factor(ipam$genotype)
-ipam$replicate <- as.factor(ipam$replicate)
+ipam$geno <- as.factor(ipam$geno)
 ipam$tank <- as.factor(ipam$tank)
-ipam$AOI <- as.factor(ipam$AOI)
 ipam$time=factor(ipam$time, levels=c("Initial", "0", "3", "7", "10", "14", "17", "21", "24", "28", "31", "35", "39", "42", "45")) 
 ipam$site=factor(ipam$site, levels=c("Emerald", "Rainbow", "Star", "MacN")) 
 ipam$treatment=factor(ipam$treatment, levels=c("CC", "CH", "LC", "LH")) 
@@ -58,12 +64,12 @@ dev.off()
 # somewhat linear decline in all combinations of site + treatment over time
 
 # testing for homogeneity of regression slopes
-# check that interaction terms with covariable (time) are not signficant
+# check that interaction terms with covariable (time) are not significant
 ipam_ofav_sub %>%
   anova_test(
     ratio ~ time + treatment + site + 
       treatment*site + time*treatment +
-      time*site + time*site*treatment
+      time*site + time*site*treatment 
   )
 # all time interactions nonsignificant
 
@@ -98,21 +104,21 @@ ofav_model_metrics %>%
 ssid_model_metrics %>% 
   filter(abs(.std.resid) > 3) -> ssid_outliers
 
-# remove outliers
-# ipam_ofav_sub <- ipam_ofav_sub[!rownames(ipam_ofav_sub) %in% ofav_outliers$.rownames,]
-# ipam_ssid_sub <- ipam_ssid_sub[!rownames(ipam_ssid_sub) %in% ssid_outliers$.rownames,]
+# remove outliers if needed
+ipam_ofav_sub <- ipam_ofav_sub[!rownames(ipam_ofav_sub) %in% ofav_outliers$.rownames,]
+ipam_ssid_sub <- ipam_ssid_sub[!rownames(ipam_ssid_sub) %in% ssid_outliers$.rownames,]
 
 #### ANCOVA ####
 
 ofav_aov <- ipam_ofav_sub %>% 
   anova_test(ratio ~ time + treatment*site)
-get_anova_table(ofav_aov)
-# all factors and treatment:site interaction significant
+get_anova_table(ofav_aov) # all factors and treatment:site interaction significant
+capture.output(get_anova_table(ofav_aov), file = "urban ipam ofav ancova.txt")
 
 ssid_aov <- ipam_ssid_sub %>% 
   anova_test(ratio ~ time + treatment*site)
-get_anova_table(ssid_aov)
-# all factors and treatment:site interaction significant
+get_anova_table(ssid_aov) # all factors and treatment:site interaction significant
+capture.output(get_anova_table(ssid_aov), file = "urban ipam ssid ancova.txt")
 
 
 #### subsetting for controls ####
@@ -150,7 +156,7 @@ ipam_ofav_control %>%
   )
 # all time interactions nonsignificant
 
-ipam_ofav_control %>%
+ipam_ssid_control %>%
   anova_test(
     ratio ~ time + treatment + site + 
       treatment*site + time*treatment +
@@ -182,20 +188,20 @@ ssid_model_control_metrics %>%
   filter(abs(.std.resid) > 3) -> ssid_control_outliers
 
 # remove outliers
-# ipam_ofav_sub <- ipam_ofav_sub[!rownames(ipam_ofav_sub) %in% ofav_outliers$.rownames,]
-# ipam_ssid_sub <- ipam_ssid_sub[!rownames(ipam_ssid_sub) %in% ssid_outliers$.rownames,]
+ipam_ofav_control <- ipam_ofav_control[!rownames(ipam_ofav_control) %in% ofav_control_outliers$.rownames,]
+ipam_ssid_control <- ipam_ssid_control[!rownames(ipam_ssid_control) %in% ssid_control_outliers$.rownames,]
 
 #### ANCOVA with controls ####
 
 ofav_control_aov <- ipam_ofav_control %>% 
   anova_test(ratio ~ time + treatment*site)
-get_anova_table(ofav_control_aov)
-# all factors and treatment:site interaction significant
+get_anova_table(ofav_control_aov) # all factors and treatment:site interaction significant
+capture.output(get_anova_table(ofav_control_aov), file = "urban ipam ofav controls ancova.txt")
 
 ssid_control_aov <- ipam_ssid_control %>% 
   anova_test(ratio ~ time + treatment*site)
-get_anova_table(ssid_control_aov)
-# time and treatment significant
+get_anova_table(ssid_control_aov) # time and treatment significant
+capture.output(get_anova_table(ssid_control_aov), file = "urban ipam ssid controls ancova.txt")
 
 
 #### subsetting for heat ####
@@ -221,7 +227,6 @@ ggscatter(
   ipam_ofav_heat, x = "time", y = "ratio",
   facet.by  = c("treatment", "site")) +
   geom_smooth(formula = y ~ x, method = "loess", span = 0.9)
-
 # somewhat linear decline in all combinations of site + treatment over time
 
 # testing for homogeneity of regression slopes
@@ -234,7 +239,7 @@ ipam_ofav_heat %>%
   )
 # time:treatment interaction significant
 
-ipam_ofav_heat %>%
+ipam_ssid_heat %>%
   anova_test(
     ratio ~ time + treatment + site + 
       treatment*site + time*treatment +
@@ -266,30 +271,30 @@ ssid_model_heat_metrics %>%
   filter(abs(.std.resid) > 3) -> ssid_heat_outliers
 
 # remove outliers
-# ipam_ofav_sub <- ipam_ofav_sub[!rownames(ipam_ofav_sub) %in% ofav_outliers$.rownames,]
-# ipam_ssid_sub <- ipam_ssid_sub[!rownames(ipam_ssid_sub) %in% ssid_outliers$.rownames,]
+# ipam_ofav_heat <- ipam_ofav_heat[!rownames(ipam_ofav_heat) %in% ofav_heat_outliers$.rownames,]
+ipam_ssid_heat <- ipam_ssid_heat[!rownames(ipam_ssid_heat) %in% ssid_heat_outliers$.rownames,]
 
 #### ANCOVA with heat ####
 
 ofav_heat_aov <- ipam_ofav_heat %>% 
   anova_test(ratio ~ time + treatment*site)
-get_anova_table(ofav_heat_aov)
-# all factors and treatment:site interaction significant
+get_anova_table(ofav_heat_aov) # all factors and treatment:site interaction significant
+capture.output(get_anova_table(ofav_heat_aov), file = "urban ipam ofav heat ancova.txt")
 
 ssid_heat_aov <- ipam_ssid_heat %>% 
   anova_test(ratio ~ time + treatment*site)
-get_anova_table(ssid_heat_aov)
-# time and treatment significant
+get_anova_table(ssid_heat_aov) # time, treatment, and site significant
+capture.output(get_anova_table(ssid_heat_aov), file = "urban ipam ssid heat ancova.txt")
 
 
 #### boxplots by species ####
 fill.color<-c("#018571","#80cdc1","#dfc27d","#a6611a")
 
 # creating dummy variables for stats labels on plots
-ofav_control_time <- data.frame(time = 10,ratio = 1,lab = "time: F1,567=136.8, p<0.001*", pH = "low pH", temp="control temp")
-ofav_control_treat <- data.frame(time = 10,ratio = 1,lab = "treat: F3,567=14.8, p<0.001*", pH = "low pH", temp="control temp")
-ofav_control_site <- data.frame(time = 10,ratio = 1,lab = "site: F3,567=134.8, p<0.001*", pH = "low pH", temp="control temp")
-ofav_control_int <- data.frame(time = 10,ratio = 1,lab = "int: F9,567=3.4, p<0.001*", pH = "low pH", temp="control temp")
+ofav_control_time <- data.frame(time = 10,ratio = 1,lab = "time: F1,563=146.7, p<0.001*", pH = "low pH", temp="control temp")
+ofav_control_treat <- data.frame(time = 10,ratio = 1,lab = "treat: F3,563=15.7, p<0.001*", pH = "low pH", temp="control temp")
+ofav_control_site <- data.frame(time = 10,ratio = 1,lab = "site: F3,563=134.0, p<0.001*", pH = "low pH", temp="control temp")
+ofav_control_int <- data.frame(time = 10,ratio = 1,lab = "int: F9,563=4.6, p<0.001*", pH = "low pH", temp="control temp")
 ofav_heat_time <- data.frame(time = 10,ratio = 1,lab = "time: F1,594=348.6, p<0.001*", pH = "low pH", temp="high temp")
 ofav_heat_treat <- data.frame(time = 10,ratio = 1,lab = "treat: F1,594=21.0, p<0.001*", pH = "low pH", temp="high temp")
 ofav_heat_site <- data.frame(time = 10,ratio = 1,lab = "site: F3,594=98.7, p<0.001*", pH = "low pH", temp="high temp")
@@ -321,12 +326,12 @@ ggsave("urban ipam ofav.pdf", ofav, width=12, height=6,dpi = 300)
 
 
 # creating dummy variables for stats labels on plots
-ssid_control_time <- data.frame(time = 10,ratio = 1,lab = "time: F1,651=20.9, p<0.001*", pH = "low pH", temp="control temp")
-ssid_control_treat <- data.frame(time = 10,ratio = 1,lab = "treat: F3,651=88.1, p<0.001*", pH = "low pH", temp="control temp")
-ssid_control_site <- data.frame(time = 10,ratio = 1,lab = "site: F3,651=1.6, p=0.19", pH = "low pH", temp="control temp")
-ssid_heat_time <- data.frame(time = 10,ratio = 1,lab = "time: F1,703=7.2, p=0.007*", pH = "low pH", temp="high temp")
-ssid_heat_treat <- data.frame(time = 10,ratio = 1,lab = "treat: F1,703=77.3, p<0.001*", pH = "low pH", temp="high temp")
-ssid_heat_site <- data.frame(time = 10,ratio = 1,lab = "site: F3,703=5.5, p=0.001*", pH = "low pH", temp="high temp")
+ssid_control_time <- data.frame(time = 10,ratio = 1,lab = "time: F1,644=35.0, p<0.001*", pH = "low pH", temp="control temp")
+ssid_control_treat <- data.frame(time = 10,ratio = 1,lab = "treat: F3,644=115.1, p<0.001*", pH = "low pH", temp="control temp")
+ssid_control_site <- data.frame(time = 10,ratio = 1,lab = "site: F3,644=1.4, p=0.24", pH = "low pH", temp="control temp")
+ssid_heat_time <- data.frame(time = 10,ratio = 1,lab = "time: F1,697=6.7, p=0.01*", pH = "low pH", temp="high temp")
+ssid_heat_treat <- data.frame(time = 10,ratio = 1,lab = "treat: F1,697=76.1, p<0.001*", pH = "low pH", temp="high temp")
+ssid_heat_site <- data.frame(time = 10,ratio = 1,lab = "site: F3,697=5.2, p=0.001*", pH = "low pH", temp="high temp")
 
 ssid <- ggboxplot(ipam_ssid,
                   x = "time",
--- stresstesting/urban tank treatments.R
+++ stresstesting/urban tank treatments.R
@@ -6,7 +6,7 @@ library(ggpubr)
 
 #### temp data import ####
 
-templog <- read.csv(file="urban_stress_temp.csv", head=T) 
+templog <- read.csv(file="urban temp.csv", head=T) 
 templog$Time<- lubridate::mdy_hms(templog$Time) # reformats time as month day year hours minutes seconds
 templog$OA <- as.factor(templog$OA)
 templog$Thermal <- as.factor(templog$Thermal)
@@ -44,13 +44,13 @@ temp_melt %>%
   filter(Thermal =="thermal") %>%
   group_by(temp)  %>% 
   summarise(avg_temp = mean(measurement, na.rm=TRUE), sd = sd(measurement, na.rm=TRUE), n = n(), se = sd/sqrt(n)) -> temp_mean # table of treatment means
-write.csv(temp_mean, file = "temp_mean.csv")
+write.csv(temp_mean, file = "urban temp mean.csv")
 
 #### ph data import ####
 
-phlog <- read.csv(file="urban_stress_ph.csv", head=T) 
+phlog <- read.csv(file="urban ph.csv", head=T) 
 phlog$Time<- lubridate::mdy_hms(phlog$Time)
-phlog$Timepoint <- as.factor(phlog$Timepoint)
+phlog$Thermal <- as.factor(phlog$Thermal)
 str(phlog)
 
 ph_melt <- gather(phlog, spp.ph.temp, measurement, Ssid.C.C:Ofav.L.H, factor_key=TRUE)
@@ -85,13 +85,13 @@ ph_melt %>%
   filter(OA =="OA") %>%
   group_by(hour, ph) %>% 
   summarise(avg_ph = mean(measurement, na.rm=TRUE), sd = sd(measurement, na.rm=TRUE), n = n(), se = sd/sqrt(n)) -> ph_mean_hour # table of treatment means by hour
-write.csv(ph_mean_hour, file = "ph_mean_hour.csv")
+write.csv(ph_mean_hour, file = "urban ph mean hour.csv")
 
 ph_melt %>% 
   filter(OA =="OA") %>%
   group_by(ph) %>% 
   summarise(avg_ph = mean(measurement, na.rm=TRUE), sd = sd(measurement, na.rm=TRUE), n = n(), se = sd/sqrt(n)) -> ph_mean
-write.csv(ph_mean, file = "ph_mean.csv")
+write.csv(ph_mean, file = "urban ph mean.csv")
 
 
 #### multi plot ####
@@ -104,4 +104,32 @@ ph_table_hour <- ggtexttable(ph_mean_hour)
 
 multiplot <- ggarrange(temp_plot, ph_plot, temp_table, ph_table, heights = c(3,1))
 multiplot
-ggsave("urban tank treatments.pdf", plot = multiplot, dpi = 300, width = 12, height = 6)
+ggsave("urban tank treatments.pdf", plot = multiplot, dpi = 300, width = 8.5, height = 4)
+
+
+#### temp and pH tank means ####
+
+ph_melt %>% # pH tank means during OA
+  filter(OA =="OA") %>%
+  group_by(species, ph, temp) %>% 
+  summarise(meanph_OA = mean(measurement, na.rm=TRUE), sdph_OA = sd(measurement, na.rm=TRUE), nph_OA = n(), seph_OA = sdph_OA/sqrt(nph_OA)) -> ph_mean_tank_OA 
+
+temp_melt %>% # temp tank means pre-thermal stress
+  filter(Thermal =="control") %>%
+  mutate(species = str_replace(species, "Ofav2", "Ofav")) %>%
+  mutate(species = str_replace(species, "Ssid2", "Ssid")) %>%
+  group_by(species, ph, temp)  %>% 
+  summarise(meantemp_control = mean(measurement, na.rm=TRUE), sdtemp_control = sd(measurement, na.rm=TRUE), ntemp_control = n(), setemp_control = sdtemp_control/sqrt(ntemp_control)) -> temp_mean_tank_control
+
+temp_melt %>% # temp tank means during thermal stress
+  filter(Thermal =="thermal") %>%
+  mutate(species = str_replace(species, "Ofav2", "Ofav")) %>%
+  mutate(species = str_replace(species, "Ssid2", "Ssid")) %>%
+  group_by(species, ph, temp)  %>% 
+  summarise(meantemp_thermal = mean(measurement, na.rm=TRUE), sdtemp_thermal = sd(measurement, na.rm=TRUE), ntemp_thermal = n(), setemp_thermal = sdtemp_thermal/sqrt(ntemp_thermal)) -> temp_mean_tank_thermal 
+
+ph_mean_tank_OA %>% # joining all the summary stats into one table
+  left_join(temp_mean_tank_control, by=c('species'='species','ph'='ph','temp'='temp')) %>%
+  left_join(temp_mean_tank_thermal, by=c('species'='species','ph'='ph','temp'='temp')) -> ph_mean_tank
+
+write.csv(ph_mean_tank, file = "urban tank treatment means.csv")
